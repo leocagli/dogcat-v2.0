a58b52a6aee3eded1e287ee57e626d824faff97c
leocagli <71848659+leocagli@users.noreply.github.com>
2025-09-10 20:06:40 -0300

    Fix TypeScript imports and missing UI components

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import type { ThemeProviderProps as NextThemeProviderProps } from 'next-themes'
+
+interface ThemeProviderProps extends NextThemeProviderProps {
+  children: React.ReactNode
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
